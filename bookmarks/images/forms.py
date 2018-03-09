1d1eeda9405f5af0973031ac1156c49a80437523
zhouhao_yjy <290322402@qq.com>
2018-03-09 12:33:30 +0800

    ch5-1

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -20,7 +20,9 @@ __mtime__ = '2018/3/8'
                   ┗┻┛  ┗┻┛
 """
 
-
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 from django import forms
 from .models import Image
 
@@ -29,4 +31,27 @@ class ImageCreateForm(forms.ModelForm):
         model = Image
         fields = ('title','url','description')
         widgets = {'url':forms.HiddenInput}
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions= ['jpg','jpeg']
+        extension = url.rsplit('.',1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('链接飞走了')
+        return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super(ImageCreateForm, self).save(commit=False)
+        image_url = self.cleaned_data['url']
+        image_name = '{}.{}'.format(slugify(image.title)),
+        image_url.rsplit('.',1)[1].lower()
+
+        #给定url中下载图片
+        response = request.urlopen(image_url)
+        image.image.save(image_name,ContentFile(response.read()),save=False)
+
+        if commit:
+            image.save()
+
+        return image
 
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 
 # Create your views here.
+
+@login_required
+def image_create(request):
+    """使用图片的视图函数"""
+    if request.method == 'POST':
+        form = ImageCreateForm(date=request.POST)
+        if form.cleaned_data
+            new_item = form
