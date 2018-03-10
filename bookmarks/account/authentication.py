14dcc41a65cc1d5f8cedc6b71528afdae4150aba
zhouhao_HN <290322402@qq.com>
2018-03-10 09:28:43 +0800

    clocal

---
+++ bookmarks/account/authentication.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'Administrator'
+__mtime__ = '2018/3/7'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+"""
+
+from django.contrib.auth.models import User
+
+
+class EmailAuthBackend(object):
+    """
+    使用邮箱认证
+    """
+    def authenticate(self,username=None,password=None):
+        try:
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
---
+++ bookmarks/account/urls.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+
+from django.conf.urls import url
+from django.contrib.auth.views import login, logout_then_login, logout, password_change_done, password_change, \
+    password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+from . import views
+
+
+
+urlpatterns = [
+    # post views
+    # url(r'^login/$', views.user_login, name='login'),
+
+    # login logout
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
+
+    # 修改密码
+    url(r'^password-change/$', password_change, name='password_change'),
+    url(r'^password-change/done/$', password_change_done, name='password_change_done'),
+
+    #  重置密码reset password
+    ## restore password urls
+    url(r'^password-reset/$',
+        password_reset,
+        name='password_reset'),
+    url(r'^password-reset/done/$',
+        password_reset_done,
+        name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+        password_reset_confirm,
+        name='password_reset_confirm'),
+    url(r'^password-reset/complete/$',
+        password_reset_complete,
+        name='password_reset_complete'),
+
+    #dashboard 的 view
+    url(r'^$', views.dashboard, name='dashboard'),
+
+    #用户注册
+    url(r'^register/$',views.register,name='register'),
+    #path(r'register/',views.register,name='register'),
+
+    #编辑他们的pfofile
+    url(r'^edit/$', views.edit, name='edit'),
+
+]
