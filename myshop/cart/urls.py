e7ce75bcb220044c6b3e285bca4740f5a2b76e76
zhouhao_yjy <290322402@qq.com>
2018-03-20 20:23:49 +0800

    ch7-2

--- myshop/cart/urls.py
+++ myshop/cart/urls.py
@@ -29,9 +29,7 @@ from . import views
 urlpatterns = [
 
     path(r'', views.cart_detail, name='cart_detail'),
-    path(r'add/(?P<product_id>\d+)/',
-         views.cart_add,
-         name='cart_add'),
+    url(r'^add/(?P<product_id>\d+)/$', views.cart_add, name='cart_add'),
 
     path(r'remove/(P<product_id>\d+)/',
          views.cart_remove,
---
+++ myshop/orders/views.py
@@ -0,0 +1,37 @@
+from decimal import Decimal
+from django.shortcuts import render
+
+
+
+# Create your views here.
+from cart.cart import Cart
+from .forms import OrderCreateForm
+from .models import OrderItem
+
+
+def order_create(request):
+    #获取到当前会话中的购物车
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+
+
+                #清空购物车
+                cart.clear()
+                #order_created.delay(order.id)
+                return render(request,
+                              'orders/order/created.html',
+                              {'order':order})
+
+    else:
+        form = OrderCreateForm()
+    return render(request,
+                  'orders/order/create.html',
+                  {'cart':cart,'form':form})
